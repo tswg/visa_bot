4b9e00bed7873e20f637ca0671cff8def46a0f0a
Papa <myhappy13@yandex.ru>
2025-11-23 16:48:27 +0300

    db config change

---
+++ src/main/java/com/example/visabot/config/TelegramBotConfig.java
@@ -0,0 +1,26 @@
+package com.example.visabot.config;
+
+import com.example.visabot.telegram.TelegramBot;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class TelegramBotConfig {
+
+    private final TelegramBot telegramBot;
+
+    @Bean
+    public TelegramBotsApi telegramBotsApi() throws TelegramApiException {
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        botsApi.registerBot(telegramBot);
+        log.info("Telegram bot registered: {}", telegramBot.getBotUsername());
+        return botsApi;
+    }
+}
--- src/main/java/com/example/visabot/config/VisaCenterDataInitializer.java
+++ src/main/java/com/example/visabot/config/VisaCenterDataInitializer.java
@@ -5,10 +5,12 @@ import com.example.visabot.repository.VisaCenterRepository;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class VisaCenterDataInitializer implements CommandLineRunner {
